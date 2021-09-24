1ab681675bf8ea44a08a8748111e1b9d6a4c58d9
Pavan699 <pavannakate03@gmail.com>
2021-09-24 08:38:35 -0700

    Adding multiple values and printing it

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -12,6 +12,16 @@ namespace BinarySearchTree
             bstm.Insert(56);
             bstm.Insert(30);
             bstm.Insert(70);
+            bstm.Insert(22);
+            bstm.Insert(95);
+            bstm.Insert(40);
+            bstm.Insert(60);
+            bstm.Insert(11);
+            bstm.Insert(65);
+            bstm.Insert(67);
+            bstm.Insert(3);
+            bstm.Insert(16);
+            bstm.Insert(63);
 
             bstm.Print();
         }
